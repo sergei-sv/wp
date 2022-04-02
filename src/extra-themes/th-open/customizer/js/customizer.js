22c2ba7978e52994f286ef421bfd0003ab03a517
Sergei Svinobaev <sergei.svinobaev@gmail.com>
2022-04-03 00:17:52 +0300

    init

--- src/extra-themes/th-open/customizer/js/customizer.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function(){
-jQuery('input[id=open_shop_main_header_layout-mhdrthree],#_customize-input-open_shop_main_header_option option[value="button"],#_customize-input-open_shop_main_header_option option[value="widget"]').prop("disabled", false);  
-});
